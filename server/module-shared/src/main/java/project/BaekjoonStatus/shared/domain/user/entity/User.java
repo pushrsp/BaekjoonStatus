5eac7e31595f7844ba21699f893a341f1a5b9930
PUSHRSP <pushrsp@gmail.com>
2023-03-05 12:00:31 +0900

    #6 - USER 엔티티

---
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/user/entity/User.java
@@ -0,0 +1,53 @@
+package project.BaekjoonStatus.shared.domain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "USER", uniqueConstraints = {
+        @UniqueConstraint(
+                name = "username_unique",
+                columnNames = "username"
+        )
+})
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "user_id", columnDefinition = "BINARY(16)")
+    private UUID id;
+
+    @Column(name = "username", length = 20, nullable = false)
+    private String username;
+
+    @Column(name = "password", length = 50, nullable = false)
+    private String password;
+
+    @Column(name = "baekjoon_username", length = 20, nullable = false)
+    private String baekjoonUsername;
+
+    @Column(name = "is_private", nullable = false)
+    private boolean isPrivate;
+
+    @Column(name = "created_time", nullable = false)
+    @CreatedDate
+    private LocalDateTime createdTime;
+
+    @Column(name = "modified_time", nullable = false)
+    @LastModifiedDate
+    private LocalDateTime modifiedTime;
+
+}
