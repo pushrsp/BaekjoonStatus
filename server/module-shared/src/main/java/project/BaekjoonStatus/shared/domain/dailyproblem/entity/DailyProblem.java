bfb8ca63e00b72aef13854ca9d4d010c17570fdc
PUSHRSP <pushrsp@gmail.com>
2023-03-06 18:16:22 +0900

    #17 - 데일리 문제 엔티티 코드 수정 및 repository 추가
    향후 추가 예정

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/dailyproblem/entity/DailyProblem.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/dailyproblem/entity/DailyProblem.java
@@ -1,6 +1,7 @@
 package project.BaekjoonStatus.shared.domain.dailyproblem.entity;
 
 import lombok.Data;
+import lombok.Getter;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
@@ -13,7 +14,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "DAILY_PROBLEM")
-@Data
+@Getter
 public class DailyProblem {
     @Id
     @GeneratedValue
