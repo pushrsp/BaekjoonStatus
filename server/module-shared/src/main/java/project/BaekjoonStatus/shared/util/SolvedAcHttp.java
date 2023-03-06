f91d152b2ae20c183e7bb94f9d291c7c234fd6e4
PUSHRSP <pushrsp@gmail.com>
2023-03-06 18:09:54 +0900

    #17 - ID 목록별 문제리스트 API 추가

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/SolvedAcHttp.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/SolvedAcHttp.java
@@ -3,19 +3,25 @@ package project.BaekjoonStatus.shared.util;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
-import project.BaekjoonStatus.shared.dto.SolvedAcProblemResp;
-import project.BaekjoonStatus.shared.dto.SolvedAcUserResp;
+import project.BaekjoonStatus.shared.dto.response.SolvedAcProblemResp;
+import project.BaekjoonStatus.shared.dto.response.SolvedAcUserResp;
 import project.BaekjoonStatus.shared.enums.CodeEnum;
 import project.BaekjoonStatus.shared.exception.SolvedAcProblemNotFound;
 import project.BaekjoonStatus.shared.exception.SolvedAcUserNotFound;
 
 import java.net.URI;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 
 public class SolvedAcHttp {
     private static final String SOLVED_AC_URL = "https://solved.ac/api/v3";
     private static final String SOLVED_AC_GET_USER_PATH = "/user/show";
     private static final String SOLVED_AC_GET_PROBLEM_PATH = "/problem/show";
+    private static final String SOLVED_AC_GET_PROBLEMS_PATH = "/problem/lookup";
+    private static final int SOLVED_AC_MAX_LEN = 100;
 
     public SolvedAcUserResp getBaekjoonUser(String baekjoonUsername) {
         URI uri = UriComponentsBuilder
@@ -49,5 +55,35 @@ public class SolvedAcHttp {
         } catch (HttpClientErrorException e) {
             throw new SolvedAcProblemNotFound(CodeEnum.SOLVED_AC_PROBLEM_NOT_FOUND);
         }
+    }
+
+    private List<SolvedAcProblemResp> getProblemsByProblemIds(String ids) {
+        URI uri = UriComponentsBuilder
+                .fromHttpUrl(SOLVED_AC_URL)
+                .path(SOLVED_AC_GET_PROBLEMS_PATH)
+                .queryParam("problemIds", ids)
+                .encode(Charset.defaultCharset())
+                .build()
+                .toUri();
+
+        //TODO 예외처리
+        RestTemplate restTemplate = new RestTemplate();
+        return Arrays.stream(Objects.requireNonNull(restTemplate.getForObject(uri, SolvedAcProblemResp[].class))).toList();
+    }
+
+    public List<SolvedAcProblemResp> getProblemsByProblemIds(List<Long> problemIds) {
+        List<SolvedAcProblemResp> problems = new ArrayList<>();
+        int offset = 0;
+        for (int i = 0; i < (problemIds.size() / SOLVED_AC_MAX_LEN) + 1; i++) {
+            List<String> ids = problemIds.subList(offset, Math.min(problemIds.size(), offset + SOLVED_AC_MAX_LEN))
+                    .stream()
+                    .map(Object::toString)
+                    .toList();
+
+            problems.addAll(this.getProblemsByProblemIds(String.join(",", ids)));
+            offset += SOLVED_AC_MAX_LEN;
+        }
+
+        return problems;
     }
 }
