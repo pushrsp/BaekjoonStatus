32a33173e8e67d8b1989a6b367bdd254ee5c038f
PUSHRSP <pushrsp@gmail.com>
2023-03-20 14:29:38 +0900

    #37 - 차트 추가

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Box } from '@mui/material'
 
-import BreadCrumb from '../components/common/BreadCrumb'
+import RadarChart from '../components/dashboard/RadarChart'
+import PieChart from '../components/dashboard/PieChart'
+import CalendarChart from '../components/dashboard/CalendarChart'
 
 const Dashboard = () => {
     return (
-        <Box m="20px">
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-                <BreadCrumb title="DASHBOARD" subtitle="Welcome to dashboard" />
+        <Box m="20px" height="100%">
+            <Box width="100%" height="50%" display="flex" justifyContent="center">
+                <CalendarChart />
+            </Box>
+            <Box width="100%" height="50%" display="flex" justifyContent="space-between">
+                <Box height="100%" width="30%" margin={0}>
+                    <RadarChart />
+                </Box>
+                <Box height="100%" width="30%" margin={0}>
+                    <PieChart />
+                </Box>
+                <Box height="100%" width="30%" margin={0}>
+                    <PieChart />
+                </Box>
+            </Box>
+            <Box width="100%" height="50%" display="flex" justifyContent="space-around">
+                <Box height="100%" width="30%" margin={0}>
+                    <RadarChart />
+                </Box>
+                <Box height="100%" width="30%" margin={0}>
+                    <PieChart />
+                </Box>
             </Box>
         </Box>
     )
