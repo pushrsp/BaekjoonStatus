c1d84df6ae9a659028aaa5522254e45d01cfbded
PUSHRSP <pushrsp@gmail.com>
2023-03-22 21:40:55 +0900

    #36 - findByUsername 추가

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/user/service/UserService.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/user/service/UserService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import project.BaekjoonStatus.shared.domain.user.entity.User;
 import project.BaekjoonStatus.shared.domain.user.repository.UserJpaRepository;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -12,6 +14,10 @@ public class UserService {
 
     public boolean exist(String username) {
         return userJpaRepository.existsByUsername(username);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userJpaRepository.findByUsername(username);
     }
 
     public User save(String username, String baekjoonUsername, String password) {
