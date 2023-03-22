7ec76b052c40b5c5fe054bb9c9c226269d3b5249
PUSHRSP <pushrsp@gmail.com>
2023-03-22 21:39:23 +0900

    #36 - 스프링 component로 관리된던걸 static 으로 변경

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/BcryptProvider.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/BcryptProvider.java
@@ -1,15 +1,13 @@
 package project.BaekjoonStatus.shared.util;
 
 import org.mindrot.jbcrypt.BCrypt;
-import org.springframework.stereotype.Component;
 
-@Component
 public class BcryptProvider {
-    public String hashPassword(String plainPassword) {
+    public static String hashPassword(String plainPassword) {
         return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
     }
 
-    public boolean validPassword(String plainPassword, String hashedPassword) {
+    public static boolean validPassword(String plainPassword, String hashedPassword) {
         return BCrypt.checkpw(plainPassword, hashedPassword);
     }
 }
