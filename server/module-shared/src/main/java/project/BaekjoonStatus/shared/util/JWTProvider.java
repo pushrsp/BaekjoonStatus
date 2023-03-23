7afdcfa240905d512b7ec9c6f465dae7be06a5b3
PUSHRSP <pushrsp@gmail.com>
2023-03-23 16:34:35 +0900

    #44 - JWTProvider 코드 개선

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/JWTProvider.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/JWTProvider.java
@@ -2,28 +2,44 @@ package project.BaekjoonStatus.shared.util;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import project.BaekjoonStatus.shared.enums.CodeEnum;
+import project.BaekjoonStatus.shared.exception.MyException;
 
+import java.io.IOException;
 import java.util.Date;
 
-@Component
 public class JWTProvider {
-    @Value("${token.secret}")
-    private String secret;
-
     private static final Long EXPIRE_TIME = 1000L * 60 * 60 * 24; //하루
 
-    public String generateToken(String userId) {
+    public static String generateToken(String userId, String secret) {
         return JWT.create()
                 .withClaim("id", userId)
-                .withExpiresAt(new Date(System.currentTimeMillis() + 3000))
+                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRE_TIME))
                 .sign(Algorithm.HMAC256(secret));
     }
 
-    public String validateToken(String token) {
-        DecodedJWT info = JWT.require(Algorithm.HMAC256(secret)).build().verify(token);
-        return info.getClaim("id").toString();
+    public static String validateToken(String authorization, String secret) {
+        if(authorization.isEmpty())
+            throw new MyException(CodeEnum.MY_SERVER_UNAUTHORIZED);
+
+        String[] token = authorization.split(" ");
+        if(token.length != 2)
+            throw new MyException(CodeEnum.MY_SERVER_UNAUTHORIZED);
+
+        try {
+            DecodedJWT info = JWT.require(Algorithm.HMAC256(secret)).build().verify(token[1]);
+            return info.getClaim("id").asString();
+        } catch (TokenExpiredException e) {
+            throw new MyException(CodeEnum.MY_SERVER_TOKEN_EXPIRED);
+        } catch (JWTDecodeException e) {
+            throw new MyException(CodeEnum.MY_SERVER_NOT_VALID_TOKEN);
+        }
     }
 }
