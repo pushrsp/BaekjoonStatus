1b034586e814bae665f270af7a85d331cae7e354
PUSHRSP <pushrsp@gmail.com>
2023-03-23 16:34:49 +0900

    #44 - solved history dto 변경

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/dto/SolvedHistoryDto.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/dto/SolvedHistoryDto.java
@@ -5,39 +5,35 @@ import lombok.*;
 public class SolvedHistoryDto {
 
     @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CountByLevel {
+        private String level;
+        private Long count;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CountByTag {
+        private String tag;
+        private Long count;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CountByDate {
+        private String day;
+        private Long value;
+    }
+
+    @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    @Builder
-    public static class SolvedHistoryResp {
+    public static class SolvedHistoryByUserId {
         private Long problemId;
         private String title;
         private Integer level;
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class SolvedCountByDate {
-        private String date;
-        private Long count;
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class SolvedCountByLevel {
-        private Integer level;
-        private Long count;
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Builder
-    public static class SolvedCountByTag {
-        private String tag;
-        private Long count;
     }
 }
