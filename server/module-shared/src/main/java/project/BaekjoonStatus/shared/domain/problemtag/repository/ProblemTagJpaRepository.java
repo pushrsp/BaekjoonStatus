ab1a7ee220d035fcd0a878495c225803da95330a
PUSHRSP <pushrsp@gmail.com>
2023-03-24 14:09:57 +0900

    #46 - problem_tag 테이블 삭제

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/problemtag/repository/ProblemTagJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package project.BaekjoonStatus.shared.domain.problemtag.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.BaekjoonStatus.shared.domain.problemtag.entity.ProblemTag;
-
-public interface ProblemTagJpaRepository extends JpaRepository<ProblemTag, Long> {
-}
