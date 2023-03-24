4ffb94b6e2d46a44d55b80d8f7b8bcf57df932e3
PUSHRSP <pushrsp@gmail.com>
2023-03-24 20:55:48 +0900

    #47 - daily_problem 도메인 코드 수정 및 추가

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/dailyproblem/entity/DailyProblem.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/dailyproblem/entity/DailyProblem.java
@@ -1,25 +1,29 @@
 package project.BaekjoonStatus.shared.domain.dailyproblem.entity;
 
-import lombok.Data;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 import org.springframework.data.annotation.CreatedDate;
 import project.BaekjoonStatus.shared.domain.problem.entity.Problem;
+import project.BaekjoonStatus.shared.util.DateProvider;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
 @Table(name = "DAILY_PROBLEM")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class DailyProblem {
     @Id
-    @GeneratedValue
-    @Column(name = "daily_problem_id")
-    private Long id;
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(name = "solved_history_id")
+    @Type(type = "uuid-char")
+    private UUID id;
 
     @OneToOne
     @JoinColumn(name = "problem_id", columnDefinition = "BIGINT")
@@ -28,4 +32,13 @@ public class DailyProblem {
     @Column(name = "created_date", nullable = false)
     @CreatedDate
     private LocalDate createdDate;
+
+    private DailyProblem(Problem problem) {
+        this.problem = problem;
+        this.createdDate = DateProvider.getDate();
+    }
+
+    public static DailyProblem create(Problem problem) {
+        return new DailyProblem(problem);
+    }
 }
