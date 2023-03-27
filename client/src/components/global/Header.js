649e887acec7bcaf1fc396511cb73351bd529183
PUSHRSP <pushrsp@gmail.com>
2023-03-27 15:56:09 +0900

    #57 - 로그아웃 버튼 추가

--- client/src/components/global/Header.js
+++ client/src/components/global/Header.js
@@ -2,13 +2,22 @@ import React, { useContext } from 'react'
 import { Box, IconButton, useTheme } from '@mui/material'
 import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined'
 import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined'
+import LogoutOutlined from '@mui/icons-material/LogoutOutlined'
+import { useRecoilState } from 'recoil'
 
 import { ColorModeContext, tokens } from '../../config/theme'
+import { userState } from '../../atom'
 
 const Header = () => {
     const theme = useTheme()
     const colors = tokens(theme.palette.mode)
     const colorMode = useContext(ColorModeContext)
+    const [user, setUser] = useRecoilState(userState)
+
+    const onLogout = () => {
+        window.localStorage.removeItem('@token')
+        setUser({})
+    }
 
     return (
         <Box display="flex" justifyContent="space-between" p={2}>
@@ -22,6 +31,11 @@ const Header = () => {
                         <LightModeOutlinedIcon />
                     )}
                 </IconButton>
+                {Object.keys(user).length > 0 && (
+                    <IconButton onClick={onLogout}>
+                        <LogoutOutlined />
+                    </IconButton>
+                )}
             </Box>
         </Box>
     )
