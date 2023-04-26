21278f5a555f8360b7337ca8e1b6cd0f643d16d0
PUSHRSP <pushrsp@gmail.com>
2023-04-26 10:57:49 +0900

    #77 - 도메인 코드 변경으로 인한 코드 수정

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/solvedhistory/repository/SolvedHistoryRepository.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/solvedhistory/repository/SolvedHistoryRepository.java
@@ -4,15 +4,14 @@ package project.BaekjoonStatus.shared.domain.solvedhistory.repository;
 import project.BaekjoonStatus.shared.domain.solvedhistory.entity.SolvedHistory;
 
 import java.util.List;
-import java.util.UUID;
 
 import static project.BaekjoonStatus.shared.dto.SolvedHistoryDto.*;
 
 public interface SolvedHistoryRepository {
     List<SolvedHistory> saveAll(List<SolvedHistory> solvedHistories);
-    List<CountByDate> findSolvedCountGroupByDate(UUID userId, String year);
-    List<CountByLevel> findSolvedCountGroupByLevel(UUID userId);
-    List<CountByTag> findSolvedCountGroupByTag(UUID userId);
-    List<SolvedHistory> findAllByUserId(UUID userId, int offset, int limit);
+    List<CountByDate> findSolvedCountGroupByDate(String userId, String year);
+    List<CountByLevel> findSolvedCountGroupByLevel(String userId);
+    List<CountByTag> findSolvedCountGroupByTag(String userId);
+    List<SolvedHistory> findAllByUserId(String userId, int offset, int limit);
     List<SolvedHistory> findAllByUserId(String userId);
 }
--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/solvedhistory/service/SolvedHistoryService.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/domain/solvedhistory/service/SolvedHistoryService.java
@@ -2,29 +2,19 @@ package project.BaekjoonStatus.shared.domain.solvedhistory.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import project.BaekjoonStatus.shared.domain.problem.entity.Problem;
 import project.BaekjoonStatus.shared.domain.solvedhistory.entity.SolvedHistory;
-import project.BaekjoonStatus.shared.domain.solvedhistory.repository.SolvedHistoryJpaRepository;
-import project.BaekjoonStatus.shared.domain.solvedhistory.repository.SolvedHistoryRepository;
 import project.BaekjoonStatus.shared.domain.user.entity.User;
-import project.BaekjoonStatus.shared.dto.SolvedHistoryDto.*;
 import project.BaekjoonStatus.shared.util.DateProvider;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class SolvedHistoryService {
-
-    private final SolvedHistoryJpaRepository solvedHistoryJpaRepository;
-    private final SolvedHistoryRepository solvedHistoryRepository;
-
     public SolvedHistory create(User user, Problem problem, boolean isBefore) {
         return new SolvedHistory(user, problem, isBefore, DateProvider.getDate(), DateProvider.getDateTime());
     }
@@ -39,35 +29,5 @@ public class SolvedHistoryService {
             ret.add(create(user, problem, isBefore));
 
         return ret;
-    }
-
-    @Transactional
-    public List<SolvedHistory> bulkInsert(List<SolvedHistory> solvedHistories) {
-        return solvedHistoryJpaRepository.saveAll(solvedHistories);
-    }
-
-    @Transactional
-    public List<SolvedHistory> saveAll(User user, List<Problem> problems, boolean isBefore) {
-        return bulkInsert(SolvedHistory.create(user, problems, isBefore));
-    }
-
-    public List<SolvedHistory> findAllByUserId(UUID userId) {
-        return solvedHistoryJpaRepository.findAllByUserId(userId);
-    }
-
-    public List<CountByDate> getSolvedCountGroupByDate(String userId, String year) {
-        return solvedHistoryRepository.findSolvedCountGroupByDate(UUID.fromString(userId), year);
-    }
-
-    public List<CountByLevel> getSolvedCountGroupByLevel(String userId) {
-        return solvedHistoryRepository.findSolvedCountGroupByLevel(UUID.fromString(userId));
-    }
-
-    public List<CountByTag> getSolvedCountGroupByTag(String userId) {
-        return solvedHistoryRepository.findSolvedCountGroupByTag(UUID.fromString(userId));
-    }
-
-    public List<SolvedHistory> findSolvedHistoriesByUserId(String userId, Integer offset, Integer limit) {
-        return solvedHistoryRepository.findAllByUserId(UUID.fromString(userId), offset, limit);
     }
 }
