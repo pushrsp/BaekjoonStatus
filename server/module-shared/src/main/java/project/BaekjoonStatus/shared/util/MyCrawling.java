df9925af1e490ee678f16693db51eee4e287effe
PUSHRSP <pushrsp@gmail.com>
2023-05-23 10:47:04 +0900

    #88 - 크롤링 상위 클래스 생성

---
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/util/MyCrawling.java
@@ -0,0 +1,18 @@
+package project.BaekjoonStatus.shared.util;
+
+import org.jsoup.Connection;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.List;
+
+public abstract class MyCrawling {
+    protected Connection conn;
+
+    protected void setConn(Connection conn) {
+        this.conn = conn;
+    }
+
+    public abstract List<Long> get();
+    protected abstract Elements getElements() throws IOException;
+}
