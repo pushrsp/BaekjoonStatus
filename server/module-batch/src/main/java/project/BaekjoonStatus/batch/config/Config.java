d04b7a7693aeae8d7ea1219ce0c3477b8db16430
PUSHRSP <pushrsp@gmail.com>
2023-05-24 11:42:45 +0900

    #99 - dailyProblemCrawling bean 등록

--- server/module-batch/src/main/java/project/BaekjoonStatus/batch/config/Config.java
+++ server/module-batch/src/main/java/project/BaekjoonStatus/batch/config/Config.java
@@ -2,6 +2,8 @@ package project.BaekjoonStatus.batch.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import project.BaekjoonStatus.shared.util.DailyProblemCrawling;
+import project.BaekjoonStatus.shared.util.MyCrawling;
 import project.BaekjoonStatus.shared.util.SolvedAcHttp;
 
 @Configuration
@@ -9,5 +11,10 @@ public class Config {
     @Bean
     public SolvedAcHttp solvedAcHttp() {
         return new SolvedAcHttp();
+    }
+
+    @Bean
+    public MyCrawling dailyProblemCrawling() {
+        return new DailyProblemCrawling();
     }
 }
