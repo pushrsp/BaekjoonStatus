b8412336ca84daacd7bd2606a04489366e609042
PUSHRSP <pushrsp@gmail.com>
2023-06-08 11:34:41 +0900

    #113 - 오늘의 문제 테스트 코드 이름 변경

--- server/module-shared/src/test/java/project/BaekjoonStatus/shared/util/DailyProblemCrawlingTest.java
+++ server/module-shared/src/test/java/project/BaekjoonStatus/shared/util/DailyProblemCrawlingTest.java
@@ -7,4 +7,27 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 class DailyProblemCrawlingTest {
+    @Test
+    public void dailyProblems_is_not_duplicated() throws Exception {
+        //given
+        DailyProblemCrawling crawling = new DailyProblemCrawling();
+
+        //when
+        List<Long> problemIds = crawling.get();
+
+        //then
+        assertThat(problemIds).doesNotHaveDuplicates();
+    }
+
+    @Test
+    public void dailyProblems_has_size_of_4() throws Exception {
+        //given
+        DailyProblemCrawling crawling = new DailyProblemCrawling();
+
+        //when
+        List<Long> problemIds = crawling.get();
+
+        //then
+        assertThat(problemIds).hasSize(4);
+    }
 }
