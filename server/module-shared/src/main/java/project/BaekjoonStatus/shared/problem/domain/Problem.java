9465d0c4fc278d91f3af0a78cb8ad213eb4b331c
PUSHRSP <pushrsp@gmail.com>
2023-06-11 16:21:37 +0900

    #119 - PROBLEM 도메인, 영속성 객체 분리

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/problem/domain/Problem.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/problem/domain/Problem.java
@@ -2,6 +2,7 @@ package project.BaekjoonStatus.shared.problem.domain;
 
 import lombok.Builder;
 import lombok.Getter;
+import project.BaekjoonStatus.shared.tag.domain.Tag;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,11 +12,11 @@ public class Problem {
     private final Long id;
     private final Integer level;
     private final String title;
-    private final List<Integer> tags;
+    private final List<Tag> tags;
     private final LocalDateTime createdTime;
 
     @Builder
-    public Problem(Long id, Integer level, String title, List<Integer> tags, LocalDateTime createdTime) {
+    public Problem(Long id, Integer level, String title, List<Tag> tags, LocalDateTime createdTime) {
         this.id = id;
         this.level = level;
         this.title = title;
