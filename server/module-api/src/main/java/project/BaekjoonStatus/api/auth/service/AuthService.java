e8ec71f542063af44b7da791cebe724abb9e74fe
PUSHRSP <pushrsp@gmail.com>
2023-06-11 16:25:44 +0900

    #119 - 메서드 이름 변경

--- server/module-api/src/main/java/project/BaekjoonStatus/api/auth/service/AuthService.java
+++ server/module-api/src/main/java/project/BaekjoonStatus/api/auth/service/AuthService.java
@@ -1,70 +1,57 @@
 package project.BaekjoonStatus.api.auth.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import project.BaekjoonStatus.shared.common.service.baekjoon.BaekjoonCrawling;
-import project.BaekjoonStatus.shared.common.service.solvedac.SolvedAcHttp;
-import project.BaekjoonStatus.shared.common.utils.BcryptProvider;
-import project.BaekjoonStatus.shared.common.utils.JWTProvider;
-import project.BaekjoonStatus.shared.problem.infra.ProblemEntity;
-import project.BaekjoonStatus.shared.tag.infra.TagEntity;
+import project.BaekjoonStatus.api.auth.controller.request.UserLoginRequest;
+import project.BaekjoonStatus.shared.user.controller.request.UserCreateRequest;
+import project.BaekjoonStatus.shared.baekjoon.BaekjoonService;
+import project.BaekjoonStatus.shared.solvedac.domain.SolvedAcProblem;
+import project.BaekjoonStatus.shared.solvedac.service.SolvedAcService;
 import project.BaekjoonStatus.shared.common.template.ListDividerTemplate;
 import project.BaekjoonStatus.api.auth.service.token.RegisterToken;
 import project.BaekjoonStatus.api.auth.service.token.RegisterTokenStore;
 import project.BaekjoonStatus.shared.problem.service.ProblemService;
-import project.BaekjoonStatus.shared.solvedhistory.infra.SolvedHistoryEntity;
 import project.BaekjoonStatus.shared.solvedhistory.service.SolvedHistoryService;
 import project.BaekjoonStatus.shared.tag.service.TagService;
-import project.BaekjoonStatus.shared.user.infra.UserEntity;
+import project.BaekjoonStatus.shared.user.domain.User;
 import project.BaekjoonStatus.shared.user.service.UserService;
-import project.BaekjoonStatus.shared.common.service.solvedac.response.SolvedAcProblemResponse;
 import project.BaekjoonStatus.shared.common.domain.exception.CodeEnum;
 import project.BaekjoonStatus.shared.common.domain.exception.MyException;
 
 import java.util.*;
 
-import static project.BaekjoonStatus.api.auth.controller.request.AuthDto.*;
-
 @Service
 @RequiredArgsConstructor
 public class AuthService {
     private static final int OFFSET = 100;
-    private static final Long EXPIRE_TIME = 1000L * 60 * 60 * 24; //하루
-    private static final SolvedAcHttp SOLVED_AC_HTTP = new SolvedAcHttp();
 
     private final RegisterTokenStore registerTokenStore = new RegisterTokenStore();
 
-    @Value("${token.secret}")
-    private String tokenSecret;
+    private final BaekjoonService baekjoonService;
+    private final SolvedAcService solvedAcService;
 
     private final ProblemService problemService;
     private final TagService tagService;
     private final UserService userService;
     private final SolvedHistoryService solvedHistoryService;
 
-    public LoginResp validateMe(String userId) {
-        UserEntity user = userService.findById(Long.parseLong(userId))
+    public User getById(String userId) {
+        return userService.findById(Long.parseLong(userId))
                 .orElseThrow(() -> new MyException(CodeEnum.MY_SERVER_LOGIN_BAD_REQUEST));
-
-        return LoginResp.builder()
-                .username(user.getUsername())
-                .id(user.getId().toString())
-                .build();
     }
 
-    public ValidBaekjoonUsernameResp validateBaekjoonUsername(String username) {
-        List<Long> problemIds = getProblemIds(username);
-        return ValidBaekjoonUsernameResp.builder()
-                .solvedCount(problemIds.size())
-                .registerToken(registerTokenStore.put(problemIds))
-                .build();
+    public List<Long> findByBaekjoonUsername(String baekjoonUsername) {
+        return baekjoonService.findByUsername(baekjoonUsername);
+    }
+
+    public String getRegisterToken(List<Long> problemIds) {
+        return registerTokenStore.put(problemIds);
     }
 
     @Async
-    public void createProblems(String registerTokenKey) {
-        RegisterToken token = registerTokenStore.get(registerTokenKey);
+    public void createProblems(String registerToken) {
+        RegisterToken token = registerTokenStore.get(registerToken);
         ListDividerTemplate<Long> listDivider = new ListDividerTemplate<>(OFFSET, token.getProblemIds());
 
         listDivider.execute((List<Long> ids) -> {
@@ -73,68 +60,52 @@ public class AuthService {
                 return null;
             }
 
-            List<SolvedAcProblemResponse> infos = SOLVED_AC_HTTP.getProblemsByProblemIds(notSavedIds);
-            List<ProblemEntity> problems = problemService.saveAll(ProblemEntity.ofWithInfos(infos));
-            tagService.saveAll(TagEntity.ofWithInfosAndProblems(infos, problems));
+            List<SolvedAcProblem> solvedAcProblems = solvedAcService.findByIds(notSavedIds);
+            problemService.saveAll(SolvedAcProblem.toProblemList(solvedAcProblems));
+            tagService.saveAll(SolvedAcProblem.toTagList(solvedAcProblems));
 
             return null;
         });
     }
 
-    public CreateUserDto createUser(String registerToken, String username, String baekjoonUsername, String password) {
-        validateExistedRegisterToken(registerToken);
-        validateDuplicateUsername(username);
+    public User createUser(UserCreateRequest userCreate) {
+        duplicateUsername(userCreate.getUsername());
 
-        UserEntity saveUser = userService.save(username, baekjoonUsername, BcryptProvider.hashPassword(password));
-        return CreateUserDto.builder()
-                .user(saveUser)
-                .registerTokenKey(registerToken)
-                .build();
+        return userService.save(User.from(userCreate));
     }
 
     @Async
-    public void createSolvedHistories(CreateUserDto data) {
-        RegisterToken token = registerTokenStore.get(data.getRegisterTokenKey());
+    public void createSolvedHistories(User user, String registerToken) {
+        RegisterToken token = registerTokenStore.get(registerToken);
         ListDividerTemplate<Long> listDivider = new ListDividerTemplate<>(OFFSET, token.getProblemIds());
 
-        listDivider.execute((List<Long> ids) -> {
-            solvedHistoryService.saveAll(SolvedHistoryEntity.ofWithUserAndProblems(data.getUser(), problemService.findAllByIdsIn(ids), true));
-            return null;
-        });
+        //FIXME
+//        listDivider.execute((List<Long> ids) -> {
+//            solvedHistoryService.saveAll(SolvedHistoryEntity.ofWithUserAndProblems(user, problemService.findAllByIdsIn(ids), true));
+//            return null;
+//        });
 
-        registerTokenStore.remove(data.getRegisterTokenKey());
+        registerTokenStore.remove(registerToken);
     }
 
-    public LoginResp login(String username, String password) {
-        UserEntity findUser = userService.findByUsername(username)
+    public User login(UserLoginRequest userLogin) {
+        User findUser = userService.findByUsername(userLogin.getUsername())
                 .orElseThrow(() -> new MyException(CodeEnum.MY_SERVER_LOGIN_BAD_REQUEST));
 
-        if(!BcryptProvider.validatePassword(password, findUser.getPassword())) {
-            throw new MyException(CodeEnum.MY_SERVER_LOGIN_BAD_REQUEST);
-        }
+        findUser.login(userLogin.getUsername(), userLogin.getPassword());
 
-        return LoginResp.builder()
-                .id(findUser.getId().toString())
-                .username(findUser.getUsername())
-                .token(JWTProvider.generateToken(findUser.getId().toString(), tokenSecret, EXPIRE_TIME))
-                .build();
+        return findUser;
     }
 
-    private void validateExistedRegisterToken(String registerToken) {
+    public void verifyRegisterToken(String registerToken) {
         if(!registerTokenStore.exist(registerToken)) {
             throw new MyException(CodeEnum.MY_SERVER_UNAUTHORIZED);
         }
     }
 
-    private void validateDuplicateUsername(String username) {
-        userService.findByUsername(username)
-                .ifPresent((u) -> {
+    private void duplicateUsername(String username) {
+        userService.findByUsername(username).ifPresent((u) -> {
                     throw new MyException(CodeEnum.MY_SERVER_DUPLICATE);
                 });
-    }
-
-    private List<Long> getProblemIds(String username) {
-        return new BaekjoonCrawling(username)
-                .get();
     }
 }
--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/problem/service/ProblemService.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/problem/service/ProblemService.java
@@ -3,6 +3,7 @@ package project.BaekjoonStatus.shared.problem.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.BaekjoonStatus.shared.problem.domain.Problem;
 import project.BaekjoonStatus.shared.problem.infra.ProblemEntity;
 import project.BaekjoonStatus.shared.problem.service.port.ProblemRepository;
 
@@ -14,31 +15,31 @@ public class ProblemService {
     private final ProblemRepository problemRepository;
 
     @Transactional
-    public ProblemEntity save(ProblemEntity problem) {
+    public Problem save(Problem problem) {
         return problemRepository.save(problem);
     }
 
     @Transactional
-    public List<ProblemEntity> saveAll(List<ProblemEntity> problems) {
+    public List<Problem> saveAll(List<Problem> problems) {
         return problemRepository.saveAll(problems);
     }
 
     @Transactional(readOnly = true)
-    public Optional<ProblemEntity> findById(Long id) {
+    public Optional<Problem> findById(Long id) {
         return problemRepository.findById(id);
     }
 
     @Transactional(readOnly = true)
-    public List<ProblemEntity> findAllByIdsIn(List<Long> ids) {
+    public List<Problem> findAllByIdsIn(List<Long> ids) {
         return problemRepository.findAllByIdsIn(ids);
     }
 
     @Transactional(readOnly = true)
     public List<Long> findAllByNotExistedIds(List<Long> ids) {
         Set<Long> set = new HashSet<>(ids);
-        List<ProblemEntity> existedProblems = findAllByIdsIn(ids);
+        List<Problem> existedProblems = findAllByIdsIn(ids);
 
-        for (ProblemEntity existedProblem : existedProblems) {
+        for (Problem existedProblem : existedProblems) {
             set.remove(existedProblem.getId());
         }
 
