e3ad41aef25144143617d45419fdcc0113606c8d
PUSHRSP <pushrsp@gmail.com>
2023-07-26 14:28:59 +0900

    #148 - api 모듈 bcrypt 모듈 생성, PasswordBcryptor 빈 생성

--- server/module-api/src/main/java/project/BaekjoonStatus/api/common/config/custom/CustomConfig.java
+++ server/module-api/src/main/java/project/BaekjoonStatus/api/common/config/custom/CustomConfig.java
@@ -1,13 +1,20 @@
-package project.BaekjoonStatus.api.common.config.async;
+package project.BaekjoonStatus.api.common.config.custom;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import project.BaekjoonStatus.api.common.utils.PasswordBcryptor;
 import project.BaekjoonStatus.api.concurrent.CustomThreadPoolExecutor;
+import project.BaekjoonStatus.shared.common.utils.PasswordEncryptor;
 
 @Configuration
-public class AsyncConfig {
+public class CustomConfig {
     @Bean
     public CustomThreadPoolExecutor customThreadPoolExecutor() {
         return new CustomThreadPoolExecutor(1);
+    }
+
+    @Bean
+    public PasswordEncryptor passwordEncryptor() {
+        return new PasswordBcryptor();
     }
 }
