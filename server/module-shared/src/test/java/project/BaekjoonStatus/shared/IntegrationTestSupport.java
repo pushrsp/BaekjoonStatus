551604b689192023bd7a7f17d537e98c0bae3e31
PUSHRSP <pushrsp@gmail.com>
2023-08-04 13:47:56 +0900

    #156 - 테스트 환경 구성

---
+++ server/module-shared/src/test/java/project/BaekjoonStatus/shared/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package project.BaekjoonStatus.shared;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public class IntegrationTestSupport {
+}
---
+++ server/module-shared/src/test/java/project/BaekjoonStatus/shared/SharedApplication.java
@@ -0,0 +1,11 @@
+package project.BaekjoonStatus.shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SharedApplication.class, args);
+    }
+}
