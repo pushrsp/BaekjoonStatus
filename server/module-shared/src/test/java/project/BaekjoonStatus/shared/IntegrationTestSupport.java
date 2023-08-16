ca94cd3da573c41c7af7fcbe86773f0e11452371
PUSHRSP <pushrsp@gmail.com>
2023-08-16 16:22:16 +0900

    #160 - 테스트 코드 삭제

--- server/module-shared/src/test/java/project/BaekjoonStatus/shared/IntegrationTestSupport.java
+++
@@ -1,9 +0,0 @@
-package project.BaekjoonStatus.shared;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
-@SpringBootTest
-public class IntegrationTestSupport {
-}
