7ebfcff40efd1b47d256d6681c122daaca2a5131
PUSHRSP <pushrsp@gmail.com>
2023-08-16 18:18:24 +0900

    #162 - id 형식 유효 검사 관련 예외 테스트

--- server/module-shared/src/test/java/project/BaekjoonStatus/shared/member/infra/MemberRepositoryTest.java
+++ server/module-shared/src/test/java/project/BaekjoonStatus/shared/member/infra/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import project.BaekjoonStatus.shared.IntegrationTestSupport;
+import project.BaekjoonStatus.shared.common.exception.InvalidIdFormatException;
 import project.BaekjoonStatus.shared.common.service.DateService;
 import project.BaekjoonStatus.shared.member.domain.Member;
 
@@ -49,7 +50,7 @@ class MemberRepositoryTest extends IntegrationTestSupport {
         Member savedMember = memberRepository.save(memberDomain);
 
         //when
-        Optional<Member> optionalMember = memberRepository.findById(Long.parseLong(savedMember.getId()));
+        Optional<Member> optionalMember = memberRepository.findById(savedMember.getId());
 
         //then
         assertThat(optionalMember.isPresent()).isTrue();
@@ -78,14 +79,25 @@ class MemberRepositoryTest extends IntegrationTestSupport {
         saveMembers(20);
 
         //when
-        List<Member> m1 = memberRepository.findAllGreaterThanMemberId(0L, 10);
-        List<Member> m2 = memberRepository.findAllGreaterThanMemberId(18L, 10);
-        List<Member> m3 = memberRepository.findAllGreaterThanMemberId(20L, 10);
+        List<Member> m1 = memberRepository.findAllGreaterThanMemberId("0", 10);
+        List<Member> m2 = memberRepository.findAllGreaterThanMemberId("3000", 10);
 
         //then
         assertThat(m1).hasSize(10);
-        assertThat(m2).hasSize(2);
-        assertThat(m3).hasSize(0);
+        assertThat(m2).hasSize(0);
+    }
+
+    @DisplayName("member_id가 유효한지 확인할 수 있다.")
+    @Test
+    public void can_detect_member_id_is_valid() throws Exception {
+        //given
+
+        //when
+        InvalidIdFormatException invalidIdFormatException = catchThrowableOfType(() -> memberRepository.findById("fdsafsa"), InvalidIdFormatException.class);
+
+        //then
+        assertThat(invalidIdFormatException).isNotNull();
+        assertThat(invalidIdFormatException.getMessage()).isEqualTo("id 형식이 올바르지 않습니다.");
     }
 
     private void saveMembers(int size) {
