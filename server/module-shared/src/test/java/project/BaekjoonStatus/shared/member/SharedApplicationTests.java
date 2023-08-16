a66fafaf9c221eeff330dccf688c941511801a34
PUSHRSP <pushrsp@gmail.com>
2023-08-16 17:29:05 +0900

    #162 - 로드용 실행 클래스 추가

---
+++ server/module-shared/src/test/java/project/BaekjoonStatus/shared/member/SharedApplicationTests.java
@@ -0,0 +1,11 @@
+package project.BaekjoonStatus.shared.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedApplicationTests {
+    public static void main(String[] args) {
+        SpringApplication.run(SharedApplicationTests.class, args);
+    }
+}
