3fdd6d1ea0432463e076476b773023845c0cff5a
PUSHRSP <pushrsp@gmail.com>
2023-08-16 16:37:20 +0900

    #162 - 통합 테스트 클래스 추가

---
+++ server/module-shared/src/test/java/project/BaekjoonStatus/shared/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package project.BaekjoonStatus.shared;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class IntegrationTestSupport {
+}
