7a9a14d5f64e3accc9d4b32246e6722660d34efd
PUSHRSP <pushrsp@gmail.com>
2023-08-31 16:51:47 +0900

    172 - 클래스 이름 변경

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/solvedhistory/domain/CountByTier.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/solvedhistory/domain/CountByTier.java
@@ -4,31 +4,29 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor
-public class GroupByTier implements Serializable {
+public class CountByTier {
     private String tier;
     private Long count;
 
     @Builder
-    public GroupByTier(String tier, Long count) {
+    public CountByTier(String tier, Long count) {
         this.tier = tier;
         this.count = count;
     }
 
-    public static Map<String, Long> toMap(List<GroupByTier> groupByTiers) {
+    public static Map<String, Long> toMap(List<CountByTier> groupByTiers) {
         return groupByTiers.stream()
-                .collect(Collectors.toMap(GroupByTier::getTier, GroupByTier::getCount, Long::sum));
+                .collect(Collectors.toMap(CountByTier::getTier, CountByTier::getCount, Long::sum));
     }
 
-    public static GroupByTier from(String tier, Long count) {
-        return GroupByTier.builder()
+    public static CountByTier from(String tier, Long count) {
+        return CountByTier.builder()
                 .tier(tier)
                 .count(count)
                 .build();
