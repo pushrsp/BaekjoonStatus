a2149bfed25bd871c33a231d7fa598664edb5fed
PUSHRSP <pushrsp@gmail.com>
2023-09-03 18:01:39 +0900

    #172 - NoArgsConstructor 삭제

--- server/module-shared/src/main/java/project/BaekjoonStatus/shared/solvedhistory/domain/CountByTier.java
+++ server/module-shared/src/main/java/project/BaekjoonStatus/shared/solvedhistory/domain/CountByTier.java
@@ -2,14 +2,12 @@ package project.BaekjoonStatus.shared.solvedhistory.domain;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @Getter
-@NoArgsConstructor
 public class CountByTier {
     private String tier;
     private Long count;
@@ -23,12 +21,5 @@ public class CountByTier {
     public static Map<String, Long> toMap(List<CountByTier> groupByTiers) {
         return groupByTiers.stream()
                 .collect(Collectors.toMap(CountByTier::getTier, CountByTier::getCount, Long::sum));
-    }
-
-    public static CountByTier from(String tier, Long count) {
-        return CountByTier.builder()
-                .tier(tier)
-                .count(count)
-                .build();
     }
 }
