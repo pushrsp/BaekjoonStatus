3c7c828517e6729ae71785088088b6252072da0b
PUSHRSP <pushrsp@gmail.com>
2023-11-21 12:38:27 +0900

    #183 - 주석 삭제

--- server/module-api/src/main/java/project/BaekjoonStatus/api/auth/service/request/MemberLoginServiceRequest.java
+++ server/module-api/src/main/java/project/BaekjoonStatus/api/auth/service/request/MemberLoginServiceRequest.java
@@ -13,8 +13,4 @@ public class MemberLoginServiceRequest {
         this.username = username;
         this.password = password;
     }
-
-    // TODO: validation
-    // @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,20}$", message = "아이디는 특수문자를 제외한 2~20자리여야 합니다.")
-    // @Length(min = 6, max = 16, message = "최소 6자 최대 16자 이내여야 합니다.")
 }
