85828b65a29cb784e16f95b5befd4e682ca5c370
PUSHRSP <pushrsp@gmail.com>
2023-11-21 12:15:25 +0900

    #183 - 클래스 이름 수정
    * user -> member

--- server/module-api/src/main/java/project/BaekjoonStatus/api/auth/controller/response/MemberLoginResponse.java
+++ server/module-api/src/main/java/project/BaekjoonStatus/api/auth/controller/response/MemberLoginResponse.java
@@ -5,20 +5,20 @@ import lombok.Getter;
 import project.BaekjoonStatus.shared.member.domain.Member;
 
 @Getter
-public class UserLoginResponse {
+public class MemberLoginResponse {
     private String id;
     private String username;
     private String token;
 
     @Builder
-    private UserLoginResponse(String id, String username, String token) {
+    private MemberLoginResponse(String id, String username, String token) {
         this.id = id;
         this.username = username;
         this.token = token;
     }
 
-    public static UserLoginResponse from(Member member, String token) {
-        return UserLoginResponse.builder()
+    public static MemberLoginResponse from(Member member, String token) {
+        return MemberLoginResponse.builder()
                 .id(member.getId())
                 .username(member.getUsername())
                 .token(token)
